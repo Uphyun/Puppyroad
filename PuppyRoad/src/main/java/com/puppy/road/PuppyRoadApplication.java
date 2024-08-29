f0db88bf3b8c56e37fb5a1bdab2a7078bafbd85a
Kimhyeonsik <tlr6392@naver.com>
2024-08-29 10:26:09 +0900

    20240829

---
+++ PuppyRoad/src/main/java/com/puppy/road/PuppyRoadApplication.java
@@ -0,0 +1,13 @@
+package com.puppy.road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyRoadApplication.class, args);
+	}
+
+}
