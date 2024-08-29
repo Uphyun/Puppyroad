4eb264db1e6e35a4e3b0dbde5fac02769f0c25c6
Kimhyeonsik <tlr6392@naver.com>
2024-08-29 17:31:04 +0900

    DB Test

---
+++ PuppyRoad/src/main/java/com/puppyroad/app/info/service/InfoService.java
@@ -0,0 +1,7 @@
+package com.puppyroad.app.info.service;
+
+import java.util.List;
+
+public interface InfoService {
+	public List<InfoVO> infoList();
+}
---
+++ PuppyRoad/src/main/java/com/puppyroad/app/info/service/InfoVO.java
@@ -0,0 +1,9 @@
+package com.puppyroad.app.info.service;
+
+import lombok.Data;
+
+@Data
+public class InfoVO {
+	private String aa;
+	private String bb;
+}
