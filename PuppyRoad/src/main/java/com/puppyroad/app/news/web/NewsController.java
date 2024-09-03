673df106c1ab31bb48f4ab42b5debc9e8965ae32
Kimhyeonsik <tlr6392@naver.com>
2024-09-03 17:58:18 +0900

    2024/09/03 - 첨부파일 등록완료 업데이트 미완

--- PuppyRoad/src/main/java/com/puppyroad/app/news/web/NewsController.java
+++ PuppyRoad/src/main/java/com/puppyroad/app/news/web/NewsController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -71,6 +72,7 @@ public class NewsController {
 	// 등록 - 처리
 	@PostMapping("newsInsert")
 	public String newsInsertProcess(@RequestPart MultipartFile[] files, NewsVO newsVO) {
+		List<String> imageList = new ArrayList<>();
 		
 		for(MultipartFile file : files) {
 			System.out.println("컨트롤러 도달");
@@ -84,24 +86,27 @@ public class NewsController {
 			log.debug("saveName : " + saveName);
 			
 			Path savePath = Paths.get(saveName); // path 순수 경로
-			
+	        
 			try {
-				file.transferTo(savePath); // 실제 업로드(내가 지정한 경로로 파일 이동시켜줌)
-				newsVO.setAttachedFile(fileName);
+				file.transferTo(savePath); 
+				imageList.add(fileName);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			
 		}
-		
+		newsVO.setAttachedFile(String.join(",", imageList));
 		int bno = newsService.addNews(newsVO);
+		
 		String url = null;
 		if(bno > -1) {
-			url = "redirect:newsInsert?bulletinNo=" + bno;
+			url = "redirect:newsInfo?bulletinNo=" + bno;
 		} else {
 			url = "redirect:newsList";
 		}
 		return url;
 	}
+
 	
 	// 수정 - 페이지
 	@GetMapping("newsUpdate")
@@ -113,14 +118,33 @@ public class NewsController {
 	
 	// 수정 - 처리
 	@PostMapping("newsUpdate")
-	@ResponseBody // => AJAX
-	public Map<String, Object> newsUpdateAJAXJSON(@RequestBody NewsVO newsVO){
-		return newsService.modifyNews(newsVO);
+	@ResponseBody
+	public Map<String, Object> newsUpdateAJAXJSON(NewsVO newsVO
+												,@RequestPart MultipartFile[] files){
+		
+		List<String> imageList = new ArrayList<>();
+		 if (files != null && files.length > 0) {
+		        for (MultipartFile file : files) {
+		            String fileName = file.getOriginalFilename();
+		            String saveName = uploadPath + fileName;
+		            Path savePath = Paths.get(saveName);
+
+		            try {
+		                file.transferTo(savePath);  
+		                imageList.add(fileName);
+		            } catch (IOException e) {
+		                e.printStackTrace();
+		            }
+		        }
+		        newsVO.setAttachedFile(String.join(",", imageList));
+		    }
+		    
+		    return newsService.modifyNews(newsVO);
 	}
 	
 	//삭제
 	@GetMapping("newsDelete")
-	public String boardDelete(@RequestParam Integer no) { //@RequestParam 생략가능 생략하는게 좋음
+	public String boardDelete(@RequestParam Integer no) { 
 		newsService.removeNews(no);
 		return "redirect:newsList";
 	}
