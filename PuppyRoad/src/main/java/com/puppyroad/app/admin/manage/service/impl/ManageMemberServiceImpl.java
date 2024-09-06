a6c1f57e520e6ca1da770d984ae49f641576a5c5
미안하대두 <alscjf2738@naver.com>
2024-09-06 17:37:25 +0900

    2024-09-06 GPS기반 지도JS배포버전(완), 관리자페이지 레이아웃 조정중

---
+++ PuppyRoad/src/main/java/com/puppyroad/app/admin/manage/service/impl/ManageMemberServiceImpl.java
@@ -0,0 +1,19 @@
+package com.puppyroad.app.admin.manage.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.puppyroad.app.admin.manage.service.ManageMemberService;
+import com.puppyroad.app.member.service.MemberVO;
+
+@Service
+public class ManageMemberServiceImpl implements ManageMemberService{
+
+	@Override
+	public List<MemberVO> getMemberList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
