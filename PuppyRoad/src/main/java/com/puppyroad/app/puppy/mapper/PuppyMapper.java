e469a49d8e89f3321f89dc2cb7f5ad8ec6e69f2d
Uphyun <sanghyun931028@gmail.com>
2024-09-11 19:14:42 +0900

    Merge branch 'master' into LSGH

--- PuppyRoad/src/main/java/com/puppyroad/app/puppy/mapper/PuppyMapper.java
+++ PuppyRoad/src/main/java/com/puppyroad/app/puppy/mapper/PuppyMapper.java
@@ -1,8 +1,13 @@
 package com.puppyroad.app.puppy.mapper;
 
+import java.util.List;
+
 import com.puppyroad.app.puppy.service.PuppyVO;
 
 public interface PuppyMapper {
 	
 	public int insertPuppy(PuppyVO puppyVO); // 강아지 프로필 등록
+	public List<PuppyVO> selectPuppy(String clientUserId); //본인 강아지 조회
+	public PuppyVO selectPuppyInfo(PuppyVO puppyVO); //단건조회
+	
 }
--- PuppyRoad/src/main/java/com/puppyroad/app/puppy/web/PuppyController.java
+++ PuppyRoad/src/main/java/com/puppyroad/app/puppy/web/PuppyController.java
@@ -1,11 +1,14 @@
 package com.puppyroad.app.puppy.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.puppyroad.app.puppy.service.PuppyService;
 import com.puppyroad.app.puppy.service.PuppyVO;
@@ -35,30 +38,48 @@ public class PuppyController {
 	
 	//강아지 프로필 등록 - 처리
 	@PostMapping("user/insertPuppy")
-	public String insertPuppyProcess(@Valid PuppyVO puppyVO, BindingResult bindingResult, Model model) {
+	public String insertPuppyProcess(@Valid PuppyVO puppyVO, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
 
 		String mcode = SecurityUtil.memberCode();
 		puppyVO.setClientUserId(mcode);
 		System.out.println(puppyVO.getClientUserId());
 		if(bindingResult.hasErrors()) {
 			model.addAttribute("puppyVO", puppyVO);
-			System.out.println("vaild에러");
+			
 			return "puppy/insertPuppy";
 		}else {
-			
 			String addPuppy = puppyservice.addPuppy(puppyVO);
 			if(addPuppy.equals("fail")) {
-				System.out.println("mapper문제");
-				
 				return "redirect:puppy/puppyInsert";
-				
 			}else {
-				
-				return "redirect:/"; 
+				redirectAttributes.addFlashAttribute("successMsg", "댕댕이 정상 등록 완료!");
+				return "redirect:/user/listPuppy"; 
 			}
 		}
-		
 	}
+	//강아지 리스트
+	@GetMapping("user/listPuppy")
+	public String listPuppys(String clientUserId, Model model) {
+		List<PuppyVO> list = puppyservice.ListPuppy(SecurityUtil.memberCode());
+		model.addAttribute("list", list);
+		return "puppy/selectPuppy";
+	}
+	
+	//강아지 조회
+	@GetMapping("user/getInfoPuppy")
+	public String getInfoPuppy(PuppyVO puppyVO, Model model) {
+		String mcode = SecurityUtil.memberCode();
+		puppyVO.setClientUserId(mcode);
+		PuppyVO Pvo = puppyservice.getInfoPuppy(puppyVO);
+		model.addAttribute("pvo", Pvo);
+		return "puppy/getInfoPuppy";
+	}
+	
+	//강아지 삭제
+	
+	//강아지 수정
+	
+	
 	
 	
 }//end of controller
