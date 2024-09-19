de0b3db011f173e62982b104b77e4f5a0254b7de
미안하대두 <alscjf2738@naver.com>
2024-09-19 19:14:55 +0900

    2024-09-19 실시간 gps

--- PuppyRoad/src/main/java/com/puppyroad/app/navi/service/impl/NaviServiceImpl.java
+++ PuppyRoad/src/main/java/com/puppyroad/app/navi/service/impl/NaviServiceImpl.java
@@ -1,6 +1,8 @@
 package com.puppyroad.app.navi.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,48 @@ public class NaviServiceImpl implements NaviService {
 	NaviMapper naviMapper;
 
 	@Override
+	public int getBoardNo(String writer) {
+		return naviMapper.selectBulletinNo(writer);
+	}
+
+	@Override
 	public List<NaviVO> callBackNavi(NaviVO naviVO) {
+		List<NaviVO> list = null;
 		if(naviVO.getPuppyCode() != null) {
 			naviMapper.setCallDogNavi(naviVO);
+			list = naviVO.getCNavi();
+		} else if (naviVO.getMatchCode() != 0) {
+			list = naviMapper.getMatchingList(naviVO.getMatchCode());
 		}
 
-		return naviVO.getCNavi();
+		return list;
+	}
+
+	@Override
+	public Map<String, Object> setMatching(NaviVO naviVO) {
+		boolean isSuccess = false;
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		int result = naviMapper.insertMatchingInfo(naviVO);
+		
+		if(result == 1) {
+			isSuccess = true;
+		}
+		
+		map.put("isSuccess", isSuccess);
+		map.put("naviVO", naviVO);
+		
+		return map;
+	}
+
+	@Override
+	public String endMatching(int matchCode) {
+		int result = naviMapper.deleteMatchingInfo(matchCode);
+		
+		if(result == 1) {
+			return "success";
+		}
+		return "fail";
 	}
 
 }
