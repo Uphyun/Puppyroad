99328462cdea9b22e80537e3982a8b18633fbf22
미안하대두 <alscjf2738@naver.com>
2024-09-26 18:00:28 +0900

    2024-09-26 일단 수정중

--- PuppyRoad/src/main/java/com/puppyroad/app/navi/service/NaviService.java
+++ PuppyRoad/src/main/java/com/puppyroad/app/navi/service/NaviService.java
@@ -3,7 +3,10 @@ package com.puppyroad.app.navi.service;
 import java.util.List;
 import java.util.Map;
 
+import com.puppyroad.app.puppy.service.PuppyVO;
+
 public interface NaviService {
+	public List<PuppyVO> getPuppyList(String bulletinNo);
 	public int getBoardNo(String writer);
 	public List<NaviVO> callBackNavi(NaviVO naviVO);
 	public Map<String, Object> setMatching(NaviVO naviVO);
